957fab8860028c0a2e25b1bf839c445b06c9a18c
sooham <rafizsooham@gmail.com>
2017-02-28 22:42:02 -0500

    Made adversarial generation graph and found learning rate.

--- adversarial.py
+++ adversarial.py
@@ -1,5 +1,8 @@
-# Generates adversarial images for a given class in a given dataset.
-# Use tensorflow version 0.11
+# Generates adversarial image examples
+
+# function to get correctly generated examples in test dataset
+
+# function to backpropagate on image vectors
 
 from __future__ import absolute_import
 from __future__ import division
@@ -9,227 +12,140 @@ import os
 import os.path as path
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # Import data
 from tensorflow.examples.tutorials.mnist import input_data
 
-cwd = os.getcwd()
-logdir = path.join(cwd, 'logs')
-savedir = path.join(cwd, 'save')
+import covnet_model
+from covnet_model import savedir
 
-if not path.exists(logdir):
-  os.mkdir(logdir)
-if not path.exists(savedir):
-  os.mkdir(savedir)
+########################### HYPER PARAMETERS ###################################
+# hyper parameter trials
+# 0.03 23.1 17.3 37.2 32.8 31.0 16.3 28.6
+# 0.04 29.1 28.2 28.4 19.6 30.4 20.2 15.2
+# 0.05 20.4 32.5 28.1 40.4 33.1 28.5 46.9
+# 0.06 19.2 29.7 27.0 27.1 19.3 36.2 38.8
+# 0.08 19.1 37.8 33.2 21.8 25.5 19.5 28.2
+# 0.1  29.1 35.9 37.8 33.4 36.2 40.7 30.2
+# 1.   46.2 58.2 50.7 64.9 46.3 47.8 63.1
+learning_rate = 0.04
+source = 2 # the index of class we want to take examples from
+target = 6 # the index of class we want to generate adversarial examples of
 
-# TODO: set a seed for all random operations
-
-############### HYPER-PARAMETERS ###########################
-learning_rate = 1e-4
-n_iter = 40000     # number of gradient descent iterations
-batch_size = 50    # mini-batch size from dataset
-dropout_prob = 0.65
-
-############### HELPER FUNCTIONS ###########################
-def conv2d(tensor, F, b, strides=1):
-  '''
-    Adds a convolutional layer with filter F and bias b
-    and ReLU activations. tensor needs to be of shape
-    [batch, in_height, in_width, in_channels] and filter
-    needs to be of shape
-    [filter_height, filter_width, in_channels, out_channels].
-  '''
-  tensor_conv = tf.nn.conv2d(tensor, F, strides=[1, strides, strides, 1], padding="SAME")
-  tensor_add_bias = tf.nn.bias_add(tensor_conv, b)
-  return tf.nn.relu(tensor_add_bias)
-
-def maxpool(tensor, k=2):
-  ''' Wrapper to compute maxpool with kxk receptive field. '''
-  return tf.nn.max_pool(
-    tensor, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME'
-  )
-
-def weight(shape):
-    ''' Initializes weight of given shape following truncated normal distribution. '''
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-def bias(shape):
-    ''' Initializes bias of given shape. '''
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-################# COVNET MODEL #############################
-
-def covnet(tensor, weights, biases, keep_prob):
-  '''
-    Creates a 3 convlayer + 2 fully connected layer model for the purposes of this
-    assignment. Returns the model predictions on MINST classes.
-
-    tensor - MINST input vector [-1, 784]
-    weights - dictionary of weights
-    biases - dictionary of biases
-    keep_prob - tf.float32 placeholder representing dropout probability
-  '''
-
-  # reshape input tensor to shape accepted by conv2d
-  tensor_r = tf.reshape(tensor, shape=[-1, 28, 28, 1])
-
-  # add summaries to analyze in tensorboard
-  tf.image_summary('MINST input image', tensor_r)
-
-  # first convolution layer
-  h_layer_1_conv = conv2d(tensor_r, weights['wc1'], biases['bc1'])
-
-  tf.histogram_summary('Convolution layer 1 conv', h_layer_1_conv)
-
-  h_layer_1_pool = maxpool(h_layer_1_conv)
-  tf.histogram_summary('Convolution layer 1 pool', h_layer_1_pool)
-
-  # second convolution layer
-  h_layer_2_conv = conv2d(h_layer_1_pool, weights['wc2'], biases['bc2'])
-
-  tf.histogram_summary('Convolution layer 2 conv', h_layer_2_conv)
-
-  h_layer_2_pool = maxpool(h_layer_2_conv)
-  tf.histogram_summary('Convolution layer 2 pool', h_layer_2_pool)
-
-  # fully connected layer 1
-  h_layer_2_pool_flat = tf.reshape(h_layer_2_pool, [-1, 7*7*64])
-
-  h_fully_connected = tf.nn.relu(tf.matmul(h_layer_2_pool_flat, weights['wf1']) + biases['bf1'])
-  tf.histogram_summary('Fully connected RELU 1', h_fully_connected)
-
-  h_fc1_drop = tf.nn.dropout(h_fully_connected, keep_prob)
-
-  # fully connected layer 2
-
-  h_fully_connected_2 = tf.nn.relu(tf.matmul(h_fc1_drop, weights['wf2']) + biases['bf2'])
-  tf.histogram_summary('Fully connected RELU 2', h_fully_connected_2)
-
-  h_fc2_drop = tf.nn.dropout(h_fully_connected_2, keep_prob)
+###################### BUILD ADVERSARIAL GRAPH #################################
 
 
-  # final output layer
-  out = tf.matmul(h_fc2_drop, weights['wo1']) + biases['bo1']
-  tf.histogram_summary('Model predictions', out)
+scaled_logits = tf.nn.softmax(covnet_model.pred)
 
-  return out
+src_acc = scaled_logits[0, source]
 
-################## TF Graph inputs #########################
+trgt_acc = scaled_logits[0, target]
 
-x = tf.placeholder(tf.float32, [None, 784])
-y = tf.placeholder(tf.float32, [None, 10])
-keep_prob = tf.placeholder(tf.float32)
-
-################### weights and biases #####################
-
-weights = {
-  # 5x5 conv, 1 input, 32 outputs
-  'wc1': weight([5, 5, 1, 32]),
-  # 5x5 conv, 32 inputs, 64 outputs
-  'wc2': weight([5, 5, 32, 64]),
-  # fully connected layer with 1024 hidden units and dropout
-  'wf1': weight([7*7*64, 1500]),
-  'wf2': weight([1500, 800]),
-  # output layer
-  'wo1': weight([800, 10])
-}
-
-biases = {
-  'bc1': bias([32]),
-  'bc2': bias([64]),
-  'bf1': bias([1500]),
-  'bf2': bias([800]),
-  'bo1': bias([10])
-}
-
-
-############## Loss Computation and Optimizer ##############
-
-pred = covnet(x, weights, biases, keep_prob)
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-
-optimizer = tf.train.GradientDescentOptimizer(
-  learning_rate=learning_rate
-).minimize(cross_entropy)
-
-accuracy = tf.reduce_mean(
-  tf.cast(tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1)), tf.float32)
-)
-
-################# Summary logging #########################
-
-# create a Saver object to save all variables for later use
-saver = tf.train.Saver()
-
-with tf.name_scope('Layer_1'):
-  filter_arr = tf.split(3, 32, weights['wc1'])
-  filter_arr = [tf.reshape(f, shape=[1, 5, 5, 1]) for f in filter_arr]
-  for i in range(len(filter_arr)):
-    tf.image_summary('filter_%d' % i, filter_arr[i])
-
-  tf.scalar_summary('bc1/mean', tf.reduce_mean(biases['bc1']))
-
-with tf.name_scope('Layer_2'):
-  # cannot show all images of filters due to size constraints
-  tf.scalar_summary('wc2/mean', tf.reduce_mean(weights['wc2']))
-  tf.scalar_summary('bc2/mean', tf.reduce_mean(biases['bc2']))
-
-with tf.name_scope('Full_Layer'):
-  tf.scalar_summary('wf1/mean', tf.reduce_mean(weights['wf1']))
-  tf.scalar_summary('bf1/mean', tf.reduce_mean(biases['bf1']))
-
-with tf.name_scope('Out_Layer'):
-  tf.scalar_summary('wo1/mean', tf.reduce_mean(weights['wo1']))
-  tf.scalar_summary('bo1/mean', tf.reduce_mean(biases['bo1']))
-
-tf.scalar_summary('accuracy', accuracy)
-tf.scalar_summary('cross entropy', cross_entropy)
-
-summaries = tf.merge_all_summaries()
-writer = tf.train.SummaryWriter(
-    logdir,
-    graph=optimizer.graph
-)
-
-################# Training session #########################
+# run gradient descent on element of scaled_logits vector corresponding
+# to target adversarial class.
+grad = tf.reshape(tf.gradients(trgt_acc, covnet_model.x)[0], shape=[1, 28, 28, 1])
 
 init = tf.initialize_all_variables()
+###################### SUMMARIES ###############################################
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+tf.image_summary('input', tf.reshape(covnet_model.x, shape=[1, 28, 28, 1]))
+tf.image_summary('grad', grad)
+tf.histogram_summary('grad', grad)
+tf.scalar_summary('target_accuracy', trgt_acc)
+tf.scalar_summary('source_accuracy', src_acc)
 
-with tf.Session() as sess:
-  sess.run(init)
+###################### TRAINING SCHEMA #########################################
 
-  print('Training covnet model on MINST...')
-  print('Open tensorboard to visualize.')
+if __name__ == '__main__':
+  if not path.isdir(savedir):
+    # train the model first
+    covnet_model.train()
 
-  for i in range(n_iter):
-    batch = mnist.train.next_batch(batch_size)
-    _, summ = sess.run(
-      [optimizer, summaries],
-      feed_dict={x: batch[0], y: batch[1], keep_prob: dropout_prob}
+  fname = raw_input('Enter model saved filename > ')
+  weights = path.join(savedir, fname)
+
+
+  with tf.Session() as sess:
+    sess.run(init)
+
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+    covnet_model.saver.restore(sess, weights)
+    print('Weights restored.')
+
+    summaries = tf.merge_all_summaries()
+
+    writer = tf.train.SummaryWriter(
+        covnet_model.logdir,
+        graph=sess.graph
     )
 
-    writer.add_summary(summ, global_step=i)
 
-  # done training, calculate metrics on test set
-  print("test accuracy %g" % accuracy.eval(
-    feed_dict={
-      x: mnist.test.images,
-      y: mnist.test.labels,
-      keep_prob: 1.0
-    }
-  ))
+    # get all test data with class 1
+    test_images = mnist.test.images
+    test_labels = mnist.test.labels
 
-  print("Optimization finished!")
-  writer.close()
+    twos = test_labels.argmax(axis=1) == source
 
-  # save if desired
-  if raw_input('Do you want to save model weights? [y/N]') == 'y':
-    fname = raw_input('Enter filename\n')
-    save_path = saver.save(sess, path.join(savedir, fname))
-    print('Model saved at: ' + save_path)
+    two_images = test_images[twos]
+    two_labels = test_labels[twos]
+
+    shuffle_idx = np.arange(two_images.shape[0])
+    np.random.shuffle(shuffle_idx)
+
+    two_images = two_images[shuffle_idx]
+    two_labels = two_labels[shuffle_idx]
+
+    print('Generating Adversarial Images.')
+    print('Open tensorboard to visualize.')
+
+    orig = two_images[np.newaxis, 0]
+    image = np.copy(orig)
+    label = two_labels[np.newaxis, 0]
+
+    i = 0
+    target_acc = 0.
+    while target_acc < .9:
+      source_acc, target_acc, g, summ = sess.run(
+        [src_acc, trgt_acc, grad, summaries],
+        feed_dict={covnet_model.x: image, covnet_model.y: label, covnet_model.keep_prob: 1.}
+      )
+
+      writer.add_summary(summ, global_step=i)
+
+      image = image + learning_rate * g.reshape(1, 28*28)
+
+      diff = np.abs(orig - image)
+
+      print("%d source_acc %.5f, target_acc %.5f, sum: %.5f" % (i, source_acc, target_acc, np.sum(diff)))
+
+      i += 1
+
+    print('Adversarial example generated.')
+
+    fig = plt.figure(figsize=(12, 12))
+    plt.subplot(131)
+    plt.imshow(orig.reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    plt.subplot(132)
+    plt.imshow(diff.reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    plt.subplot(133)
+    plt.imshow(image.reshape(28, 28), cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+    if raw_input('Do you want to save this example? [y\N] ') == 'y':
+      fname = raw_input('Enter name of .npy file to save to > ')
+      np.savez(
+        path.join(savedir, fname),
+        source=orig,
+        delta=diff,
+        target=image,
+        source_acc=source_acc,
+        target_acc=target_acc
+      )
+
+  covnet_model.train_sess.close()
